723882ff8977d13b4cd8bc6e875504c839e07c86
jiangbo <937158168@qq.com>
2020-07-07 20:08:52 +0800

    登录模块已经完成

--- sso-login/src/main/java/com/jb/ssologin/controller/LoginController.java
+++ sso-login/src/main/java/com/jb/ssologin/controller/LoginController.java
@@ -1,12 +1,14 @@
 package com.jb.ssologin.controller;
 
 
+import cn.hutool.db.Db;
 import com.alibaba.druid.support.json.JSONUtils;
 import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONObject;
 import com.jb.ssologin.service.DbUserService;
 import com.jb.ssologin.utils.RedisUtil;
 import com.jb.ssologin.vo.DbUser;
-import org.codehaus.jettison.json.JSONObject;
+import org.apache.catalina.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -15,9 +17,13 @@ import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
+/**
+ * @author jiangbo
+ */
 @CrossOrigin
 @Controller
 public class LoginController {
@@ -33,24 +39,38 @@ public class LoginController {
     @RequestMapping(value = "/login",method = RequestMethod.POST)
     public Map<String, Object> login(String username, String password,String uuid){
 
-        Map<String,Object> map = new HashMap<>();
-        //标识符 成功返回1 失败返回0
-        int code = 0;
-        String user = null ;
-        //判断用户提交是否携带UUID
-        if (uuid != null){//如果携带就从redis里取出
-            user = redisUtil.get(uuid+":"+username);
-        }else {
-            //没有的话就查找账号密码
-            user = JSON.toJSONString(dbUserService.findUser(new DbUser(username, password)));
-            if (user!= null){
-                code = 1;
-                uuid = UUID.randomUUID().toString();
-                redisUtil.set(uuid+":"+username, user );
-                redisUtil.expire(uuid,1000, TimeUnit.SECONDS);
-            }
-        }
+        //初始化返回数据
+       Map<String,Object> map = new HashMap<>();
+       DbUser user = null;
+       //返回状态 0是账号密码错误  1是登陆成功 3是UUID失效
+       Integer code = 0;
+        System.out.println(uuid);
 
+
+       //判断请求是否携带UUID
+       if (uuid != ""  || uuid.equals("")==false){
+           //如果携带就去redis里面查
+           JSONObject jsonObject = JSONObject.parseObject(redisUtil.get(uuid));
+           //如果在redis里面没有查到那么就返回状态 3
+           if (jsonObject == null){
+               code = 3;
+           }else {
+               //如果在redis查到那么就返回状态 1
+               user = JSON.toJavaObject(jsonObject, DbUser.class);
+               code =1;
+           }
+
+       }else {//如果没有携带UUID那么就去数据库查
+           user = dbUserService.findUser(new DbUser(username,password));
+
+           if (user !=null){
+               uuid = UUID.randomUUID().toString();
+               redisUtil.set(uuid,JSON.toJSONString(user));
+               code = 1;
+           }else {
+               code = 0;
+           }
+       }
 
         map.put("user",user);
         map.put("uuid",uuid);
